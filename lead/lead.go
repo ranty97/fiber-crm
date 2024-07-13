7706078d442b442d879ba2544ba96bfb6a865291
ranty97 <googacc9091@gmail.com>
2024-07-13 21:47:01 +0300

    lead model and stable

--- lead/lead.go
+++ lead/lead.go
@@ -1,6 +1,50 @@
 package lead
 
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/ranty97/fiber-crm-basic/database"
+)
+
 type Lead struct {
-	Age  int
-	Team string
+	gorm.Model
+	Name  string `json:"name"`
+	Team  string `json:"team"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func GetLeads(c *fiber.Ctx) {
+	db := database.DBconnection
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+}
+
+func GetLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBconnection
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+}
+
+func Delete(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBconnection
+	var lead Lead
+	db.First(&lead, id)
+	db.Delete(&lead)
+	c.JSON(lead)
+}
+
+func CreateLead(c *fiber.Ctx) {
+	db := database.DBconnection
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
 }
--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/ranty97/fiber-crm-basic/database"
+	"github.com/ranty97/fiber-crm-basic/lead"
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("api/v1/lead", GetLeads)
-	app.Post("api/v1/lead", CreateLead)
-	app.Delete("api/v1/lead/:id", DeleteLead)
-	app.Get("api/v1/lead/:id", GetLead)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Post("/api/v1/lead", lead.CreateLead)
+	app.Delete("/api/v1/lead/:id", lead.Delete)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
 }
 
 func initDB() {
-
+	var err error
+	database.DBconnection, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed connect to database")
+	}
+	fmt.Println("Opened connection to database")
+	database.DBconnection.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated")
 }
 
 func main() {
 	app := fiber.New()
+	initDB()
 	setupRoutes(app)
 	app.Listen("localhost:9091")
+	defer database.DBconnection.Close()
 }
